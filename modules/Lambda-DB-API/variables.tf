3a5a2eb3d3dd9eb6aa08b1a3c20a6148c05078b0
Tommy Chua Peng Gee <167774738+Tommycpg-ce6@users.noreply.github.com>
2024-10-15 23:12:58 +0800

    Revert "Dev"

--- modules/Lambda-DB-API/variables.tf
+++ modules/Lambda-DB-API/variables.tf
@@ -2,8 +2,3 @@ variable "env" {
   type    = string
   default = ""
 }
-
-variable "aws_region" {
-  type    = string
-  default = "ap-southeast-1" # You can change this default or pass it in via command line
-}
