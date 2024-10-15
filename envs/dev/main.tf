273533b27f55e0f178982de3fe02d230c6a23cfa
ald1ndc <aldindc@yahoo.com>
2024-10-15 17:53:42 +0800

    updated dev main.tf for source dir 5

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -6,15 +6,15 @@ locals {
 
 
 module "cloudfront-s3" {
-  source = "../modules/cloudfront-s3"
-  #source = "../../modules/cloudfront-s3"
+  #source = "../modules/cloudfront-s3"
+  source = "../../modules/Cloudfront-S3"
   env    = local.env
 }
 
 
 module "lambda-db-api" {
-  source = "../modules/lambda-db-api"
-  #source = "../../modules/lambda-db-api"
+  #source = "../modules/ambda-db-api"
+  source = "../../modules/Lambda-DB-API"
   env    = local.env
 }
 
