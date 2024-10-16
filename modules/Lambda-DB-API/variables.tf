bab84e0d3bedfa79bbb0f506c290295b63e4568a
Bengee07 <bengeeepaton@gmail.com>
2024-10-16 23:17:54 +0800

    Updated Main

--- modules/Lambda-DB-API/variables.tf
+++ modules/Lambda-DB-API/variables.tf
@@ -2,3 +2,9 @@ variable "env" {
   type    = string
   default = ""
 }
+
+variable "aws_region" {
+  description = "The AWS region where resources will be created"
+  type        = string
+  default     = "ap-southeast-1"  
+}
